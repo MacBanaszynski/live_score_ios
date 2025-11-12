febac01415b321b2f6e571f00e565490ae3335c5
MacBanaszynski <maciej.banaszynski@icloud.com>
2025-11-12 22:11:25 +0100

    Initial Commit

---
+++ LiveScoreTests/LiveScoreTests.swift
@@ -0,0 +1,16 @@
+//
+//  LiveScoreTests.swift
+//  LiveScoreTests
+//
+//  Created by Maciej Banaszyński on 12/11/2025.
+//
+
+import Testing
+
+struct LiveScoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
